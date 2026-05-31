abe19aea11afb579ed17284697b147aa8611020b
kartikey312 <kartikmishraharsh@gmail.com>
2026-05-31 23:03:11 +0530

    backend_logic changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,48 @@
-import  express  from "express";
+import express from "express";
 import cors from "cors";
+import multer from "multer";
 
 const app = express();
 
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (_, file, cb) => {
+    cb(
+      null,
+      `${Date.now()}-${file.originalname}`
+    );
+  },
+});
+
+const upload = multer({ storage });
+
 app.get("/", (_, res) => {
   res.send("Backend Running");
 });
 
+app.post(
+  "/upload",
+  upload.single("audio"),
+  (req, res) => {
+
+    console.log("File Uploaded");
+
+    console.log(req.file);
+
+    res.status(200).json({
+      success: true,
+      file: req.file?.filename,
+    });
+  }
+);
+
 app.listen(5000, () => {
-  console.log("Server running");
+  console.log(
+    "Server running on port 5000"
+  );
 });
